add1b197a10c425573e263a87b4cd7f077dcfff2
emilysupercode <sheil.emily.sc@gmail.com>
2022-11-27 20:16:29 +0100

    styling in progress

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,12 +13,12 @@ function Suspect(name, firstName, lastName, occupation, age, description, image,
     this.color = color;
 }
 
-let mrGreen = new Suspect("mrGreen", "Jacob", "Green", "Entrepreneur", 45, "He has a lot of connections", "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg", "green");
-let drOrchid = new Suspect("drOrchid", "Doctor", "Orchid", "Scientist", 26, "PhD in plant toxicology. Adopted daughter of Mr. Boddy", "http://www.radiotimes.com/uploads/images/Original/111967.jpg", "white")
-let profPlum = new Suspect("profPlum", "Victor", "Plum", "Designer", 22, "Billionaire video game designer", "https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg", "purple")
-let missScarlet = new Suspect("missScarlet", "Kasandra", "Scarlet", "Actress", 31, "She is an A-list movie star with a dark past", "https://static.standard.co.uk/s3fs-public/thumbnails/image/2016/07/04/10/cluedo-miss-scarlett.jpg", "red")
-let mrsPeacock = new Suspect("mrsPeacock", "Eleanor", "Peacock", "Socialité", 36, "She is from a wealthy family and uses her status and money to earn popularity", "https://pm1.narvii.com/6722/3bd0d86d367315d9e81a12390c17343a27d8a4d9_hq.jpg", "blue")
-let mrMustard = new Suspect("mrMustard", "Jack", "Mustard", "Retired Football player", 62, "He is a former football player who tries to get by on his former glory", "https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg", "yellow")
+let mrGreen = new Suspect("mrGreen", "Jacob", "Green", "Entrepreneur", 45, "He has a lot of connections", "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg", "#1C6758");
+let drOrchid = new Suspect("drOrchid", "Doctor", "Orchid", "Scientist", 26, "PhD in plant toxicology. Adopted daughter of Mr. Boddy", "http://www.radiotimes.com/uploads/images/Original/111967.jpg", "#FFF")
+let profPlum = new Suspect("profPlum", "Victor", "Plum", "Designer", 22, "Billionaire video game designer", "https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg", "#512C62")
+let missScarlet = new Suspect("missScarlet", "Kasandra", "Scarlet", "Actress", 31, "She is an A-list movie star with a dark past", "https://static.standard.co.uk/s3fs-public/thumbnails/image/2016/07/04/10/cluedo-miss-scarlett.jpg", "#C70D3A")
+let mrsPeacock = new Suspect("mrsPeacock", "Eleanor", "Peacock", "Socialité", 36, "She is from a wealthy family and uses her status and money to earn popularity", "https://pm1.narvii.com/6722/3bd0d86d367315d9e81a12390c17343a27d8a4d9_hq.jpg", "#115173")
+let mrMustard = new Suspect("mrMustard", "Jack", "Mustard", "Retired Football player", 62, "He is a former football player who tries to get by on his former glory", "https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg", "#FFBD39")
 
 suspectsArray.push(mrGreen, drOrchid, profPlum, missScarlet, mrsPeacock, mrMustard)
 
@@ -46,6 +46,8 @@ function selectRandom(arrayName) {
 }
 
 let whoDunitEl = document.getElementById("whoDunit");
+let answerOutput = document.createElement("p");
+whoDunitEl.appendChild(answerOutput);
 let cardRevealEl = document.getElementById("cardReveal");
 let suspectCardImgEl = document.getElementById("suspectCardImg");
 let suspectCardSuspectNameEl = document.getElementById("suspectCardSuspectName");
@@ -59,20 +61,23 @@ let suspect;
 let weapon;
 let room;
 
-function Result(suspectResult, weaponResult, roomResult) {
+function Result(suspectResult, weaponResult, roomResult, suspectColor) {
     this.suspectResult = suspectResult;
     this.weaponResult = weaponResult;
     this.roomResult = roomResult;
+    this.suspectColor = suspectColor;
 }
+
 
 function pickMystery() {
     suspect = selectRandom(suspectsArray);
     weapon = selectRandom(weaponsArray);
     room = selectRandom(roomsArray);
-    return output = new Result((suspect.firstName + " " + suspect.lastName), weapon.weaponName, room)
+    return output = new Result((suspect.firstName + " " + suspect.lastName), weapon.weaponName, room, suspect.color)
 }
 
 function revealMystery() {
     pickMystery();
-    whoDunitEl.innerHTML = `${output.suspectResult} killed Mr. Boddy using the ${weapon.weaponName} in the ${room}!`;
+    answerOutput.innerHTML = `<span style=color:${suspect.color}>${output.suspectResult}</span> <br>killed Mr. Boddy using the ${weapon.weaponName} in the ${room}!`;
+    suspectCardImgEl.src = ""
 }
